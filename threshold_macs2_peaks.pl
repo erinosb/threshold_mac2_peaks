2205dacfc50a67e48a9d34e046ef4ddf0de0568a
erinosb <erinosb@gmail.com>
2015-05-11 15:58:02 -0400

    updated to fix some bugs

--- threshold_macs2_peaks.pl
+++ threshold_macs2_peaks.pl
@@ -1,4 +1,4 @@
-#! /usr/local/bin/perl -w
+#! /usr/bin/perl
 use strict;
 use warnings;
 use Getopt::Long;
@@ -11,27 +11,43 @@ use Getopt::Long;
 #
 #Script purpose:  I typically have MACS2 output that I would like to threshold for peaks over a given score. Scores are located in the 
 #
-#INPUT: Input is
-#                       a MACS2 _peaks.bed file that looks like this:
-                    #chrI	11176	11732	MACS_peak_1	33.44
-                    #chrI	36353	36729	MACS_peak_2	11.35
-                    #chrI	38906	39187	MACS_peak_3	28.41
-                    #chrI	116506	116931	MACS_peak_4	6.46
-                    #chrI	122424	122775	MACS_peak_5	7.30
-                    #chrI	128413	128920	MACS_peak_6	5.77
-                    #chrI	167901	168157	MACS_peak_7	7.30
-                    #chrI	256399	256603	MACS_peak_8	3.47
-                    #chrI	310793	311034	MACS_peak_9	18.68
+#INPUT: Input is    EITHER a LIST file containing the name of a bed file on each line
+#                   OR a comma-separated list of .bed files.
+#
+#                   LIST:
+                        #9_EO038_over_EO042_0.005_peaks.bed
+                        #9_EO039_over_EO042_0.005_peaks.bed
+#
+#                   Each entry in the list must contain an MACS2 peaks_bed file that looks like this when opened:
+                        #chrI	11176	11732	MACS_peak_1	2.44
+                        #chrI	36353	36729	MACS_peak_2	11.35
+                        #chrI	38906	39187	MACS_peak_3	28.41
+                        #chrI	116506	116931	MACS_peak_4	3.46
+                        #chrI	122424	122775	MACS_peak_5	7.30
+                        #chrI	128413	128920	MACS_peak_6	5.77
+                        #chrI	167901	168157	MACS_peak_7	7.30
+                        #chrI	256399	256603	MACS_peak_8	3.47
+                        #chrI	310793	311034	MACS_peak_9	18.68
 #                    
-#                       a score for thresholding. If 5 is given, only entries with a score greater than 5 in the 5th columsn (-log10 score) will be captured.
+#                   SCORE:
+#                       A numeric score for thresholding.
+#                       If 5 is given, only entries with a score greater than 5 in the 5th columsn (-log10 score) will be captured.
 #
-#OUTPUT: Output is:     a _peaks_greaterN.bed
-#
+#OUTPUT: Output is:     a file1_peaks_greaterN.bed in which only the .bed entries with a score greater than the user specified score are retained:
+                        #chrI	36353	36729	MACS_peak_2	11.35
+                        #chrI	38906	39187	MACS_peak_3	28.41
+                        #chrI	122424	122775	MACS_peak_5	7.30
+                        #chrI	128413	128920	MACS_peak_6	5.77
+                        #chrI	167901	168157	MACS_peak_7	7.30
+                        #chrI	310793	311034	MACS_peak_9	18.68
+#                        
+#                        
 #USAGE:  perl threshold_macs2_peaks.pl --score <N> --list <file1.txt>
 #        OR
 #        perl threshold_macs2_peaks.pl --score <N> --bed <file1.bed,file2.bed>
 #
-#   
+#
+#BUGS:  die commands don't work.
 ################
 
   
@@ -39,53 +55,59 @@ use Getopt::Long;
 my $score = '';
 my $list = '';
 my $bed = '';
-
 GetOptions ("score=i" => \$score,
             "list=s" => \$list,
-            "bed=s" => \$bed)
-or die("Error in command line arguments\n");
-
-#print "score is $score\n";
-#print "list is $list\n";
-#print "bed is $bed\n";
+            "bed=s" => \$bed);
 
 
-if ($list) {
+
+if ($list or $bed) {
+    print "score is $score\n";
+    print "list is $list\n";
+    print "bed is $bed\n";
+}
+else{
+    die("Error in command line arguments\n. bed files or a list of bed files required");
+}
+
     
+#IF a listfile is given:
+if ($list) {
+
     #Check the list file is ok.
-    unless (open (LIST, $list)){
+    unless (open(LIST, $list)){
         print "cannot open textfile that lists _peaks.bed files to process.\n";
         exit;
     }
+        
     
     #Cycle through each entry of the list file and process each _peaks.bed file
     while (my $line = <LIST>){
         chomp $line;
         print $line, "\n";
         threshold(\$score, \$line);
-        
-        
     }
-
+    
+    close LIST;
 }
 
+#IF bed files are given...
 if ($bed){
     
+    #split apart each .bed file
     my @bedarray = split(",", $bed);
 
+    #Cycle through and process each _peaks.bed file
     foreach(@bedarray) {
         threshold(\$score, \$_);
     }
 }
 
 
-
-
-
-##############################################################################################
-# SUBROUTINES
-##############################################################################################
-#A subroutine to capture only .bed entries with scores above the specified score threshold.
+###############################################################################################
+## SUBROUTINES
+###############################################################################################
+##A subroutine to capture only .bed entries with scores above the specified score threshold.
 sub threshold {
     
     #capture referenced elements.
@@ -121,16 +143,12 @@ sub threshold {
         if ($line_array[4] >= $$score_ref) {
             print OUT $newline, "\n";
         }
-        
-        
     }
     
+    close OUT;
+    close BED;
     
 }
 
-#my $output = $time . "_HTseq_run.LOG";
-#unless (open(OUT, ">$output")) {
-#    print "cannot open output file\n";
-#    exit;
-#}
 
+__END__
